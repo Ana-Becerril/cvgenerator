30bf8d54311509c9b98740dee7d866d1f4f34131
Ana- Becerril <ana.becbel@gmail.com>
2021-06-24 14:49:35 -0500

    started with header and body on the main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import styles from './App.module.css'
 import Form from './components/Form';
 import CV from './components/CV';
 
 
 const App = () => (
   <>
-  <Router>
-    <div>
-      <h2>CV generator</h2>
-      <ul className="navbar-nav mr-auto">
-            <li><Link to={'/form'} className="nav-link"> Form </Link></li>
-            <li><Link to={'/cv'} className="nav-link">CV</Link></li>
-          </ul>
-          <Switch>
-              <Route exact path='/form' component={Form} />
-              <Route path='/cv' component={CV} />
-          </Switch>
-    </div>
+    <Router>
+      <div className={styles.appContainer}>
+        <div className={styles.header}>
+          <div className={styles.title}>CV Generator</div>
+          </div>
+        <div className={styles.navBar}>
+          <div><Link to={'/form'} className={styles.navLink}> Form </Link></div>
+          <div><Link to={'/cv'} className={styles.navLink}> CV </Link></div>
+        </div>
+        <Switch>
+          <Route exact path='/form' component={Form} />
+          <Route path='/cv' component={CV} />
+        </Switch>
+      </div>
     </Router>
   </>
 );
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import styles from './style.module.css';
 
 const Form = () => (
     <>
     <div>
-      <h2>Form</h2>
+      <div className= {styles.formContainer}>
+          <div className={styles.headerContainer}> Header</div>
+          <div className={styles.bodyContainer}>Body</div>
+          <div className={styles.footerContainer}>Footer</div>  
+      </div>
     </div>
   </>
 );
