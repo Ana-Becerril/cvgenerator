dd8c3ed2daa1d3388fe0bfb7a865427db6e7714e
Ana- Becerril <ana.becbel@gmail.com>
2021-06-24 23:00:53 -0500

    set a switch that navigate trough the components to watch the template and the result

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch as SwitchRouter, Route, Link, useLocation } from 'react-router-dom';
 import styles from './App.module.css'
 import Form from './components/Form';
 import CV from './components/CV';
+import Switch from "react-switch";
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
+const App = () => {
+  let location = useLocation();
+  const history = useHistory();
+  const [checked, setChecked] = useState(false);
+  const handleChange = nextChecked => {
+    setChecked(nextChecked);
+    if (location.pathname === '/form') history.push('/cv')
+    if (location.pathname === '/cv') history.push('/form')
+  };
 
-const App = () => (
-  <>
-    <Router>
-      <div className={styles.appContainer}>
-        <div className={styles.header}>
-          <div className={styles.title}>CV Generator</div>
+  return (
+    <>
+      <Router>
+        <div className={styles.appContainer}>
+          <div className={styles.header}>
+            <div className={styles.title}>CV Generator</div>
           </div>
-        <div className={styles.navBar}>
-          <div><Link to={'/form'} className={styles.navLink}> Form </Link></div>
-          <div><Link to={'/cv'} className={styles.navLink}> CV </Link></div>
+          <div className={styles.navBar}>
+            <SwitchRouter>
+              <Switch
+                onChange={handleChange}
+                checked={checked}
+                uncheckedIcon={false}
+                checkedIcon={false}
+                offColor="#9052aa"
+                onColor="#d6bcfa"
+                offHandleColor="#ffffff"
+                onHandleColor="#ffffff"
+              />
+              <Route exact path='/form' component={Form} />
+              <Route path='/cv' component={CV} />
+            </SwitchRouter>
+          </div>
         </div>
-        <Switch>
-          <Route exact path='/form' component={Form} />
-          <Route path='/cv' component={CV} />
-        </Switch>
-      </div>
-    </Router>
-  </>
-);
+      </Router>
+    </>
+  );
+}
 
 export default App;
