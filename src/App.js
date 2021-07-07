778cd88535859c60a4d2bc190a5acab8e68a8a47
Ana- Becerril <ana.becbel@gmail.com>
2021-07-06 20:06:36 -0500

    deleted many components to set all form in a same component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import Form from './containers/Form';
 import CV from './containers/CV';
 import Header from './containers/Header'
+import styles from './App.module.css'
 
 const App = () => {
   const [datos, setDatos] = useState({
@@ -29,15 +29,162 @@ const App = () => {
       ...datos,
       [event.target.name]: event.target.value
     })
-    }
+  }
+
+  const submitDatos = (event) => {
+    event.preventDefault()
+    console.log('enviando datos...')
+}
 
   return (
     <>
       <Header />
-      <Form
-        handleInputChange={handleInputChange} />
-      <CV 
-      datos={datos} />
+      <div className={styles.formContainer}>
+      <div className={styles.headerNameContainer}>
+        <form onSubmit={e => e.preventDefault}>
+          <h1>Personal Information</h1>
+          <input
+            type="text"
+            placeholder="First Name"
+            onChange={handleInputChange}
+            name="firstName"
+
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            onChange={handleInputChange}
+            name="lastName"
+
+          />
+          <input
+            type="text"
+            placeholder="Role"
+            onChange={handleInputChange}
+            name="role"
+
+          />
+          <input
+            type="text"
+            placeholder="Address"
+            onChange={handleInputChange}
+            name="address"
+
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={handleInputChange}
+            name="email"
+
+          />
+          <input
+            type="text"
+            placeholder="Phone"
+            onChange={handleInputChange}
+            name="phone"
+
+          />
+        </form>
+
+      </div>
+      <div className={styles.experienceContainer}>
+        <form onSubmit={e => e.preventDefault}>
+
+          <h1>Experience</h1>
+          <input
+            type="text"
+            placeholder="Position"
+            onChange={handleInputChange}
+            name="position"
+          />
+          <input
+            type="text"
+            placeholder="Company"
+            onChange={handleInputChange}
+            name="company"
+          />
+          <input
+            type="text"
+            placeholder="City"
+            onChange={handleInputChange}
+            name="city"
+          />
+          <input
+            type="text"
+            placeholder="From"
+            onChange={handleInputChange}
+            name="from"
+          />
+          <input
+            type="text"
+            placeholder="To"
+            onChange={handleInputChange}
+            name="to"
+          />
+          <button type="">
+            Add
+          </button>
+          <button>
+            Delete
+          </button>
+
+        </form>
+      </div>
+      <div className={styles.educationContainer}>
+        <form onSubmit={e => e.preventDefault}>
+
+          <h1>Education</h1>
+
+          <input
+            type="text"
+            placeholder="University Name"
+            onChange={handleInputChange}
+            name="university" />
+          <input
+            type="text"
+            placeholder="City"
+            onChange={handleInputChange}
+            name="cityEducation"
+          />
+          <input
+            type="text"
+            placeholder="Degree"
+            onChange={handleInputChange}
+            name="degree"
+          />
+          <input
+            type="text"
+            placeholder="Subject"
+            onChange={handleInputChange}
+            name="subject"
+          />
+          <input
+            type="text"
+            placeholder="From"
+            onChange={handleInputChange}
+            name="fromEducatiom"
+          />
+          <input
+            type="text"
+            placeholder="To"
+            onChange={handleInputChange}
+            name="toEducation"
+          />
+
+          <button>
+            Add
+          </button>
+          <button>
+            Delete
+          </button>
+
+        </form>
+
+      </div>
+      </div>
+      <CV
+        datos={datos} />
     </>
   );
 };
--- src/containers/CV/index.js
+++ src/containers/CV/index.js
@@ -1,27 +1,43 @@
 import React from 'react';
 import styles from './style.module.css';
 
-const CV = ({datos}) => {
+const CV = ({ datos }) => {
 
 
 
   return (
-  <>
-    <div>
-      <div className={styles.formContainer}>
-        <div className={styles.form}>
-          <div className={styles.mainContainers}>
-           <div>{datos.firstName}</div>
-           <div>{datos.lastName}</div>
-           <div>{datos.role}</div>
-           <div>{datos.address}</div>
-           <div>{datos.email}</div>
-           <div>{datos.phone}</div>
+    <>
+      <div>
+        <div className={styles.formContainer}>
+          <div className={styles.form}>
+            <div className={styles.mainContainers}>
+
+              <div className={styles.headerNameContainer}>
+                <div>{datos.firstName}</div>
+                <div>{datos.lastName}</div>
+                <div>{datos.role}</div>
+                <p>Persona</p>
+                <div>{datos.address}</div>
+                <div>{datos.email}</div>
+                <div>{datos.phone}</div>
+              </div>
+
+              <div>{datos.position}</div>
+              <div>{datos.company}</div>
+              <div>{datos.city}</div>
+              <div>{datos.from}</div>
+              <div>{datos.to}</div>
+              <div>{datos.university}</div>
+              <div>{datos.cityEducation}</div>
+              <div>{datos.degree}</div>
+              <div>{datos.subject}</div>
+              <div>{datos.fromEducation}</div>
+              <div>{datos.toEducation}</div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </>
+    </>
   );
 };
 
