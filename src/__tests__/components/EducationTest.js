b752ef462e41b5c9cc41bc9bac220c1562dc9686
Ana- Becerril <ana.becbel@gmail.com>
2021-08-14 13:04:28 -0500

    test

---
+++ src/__tests__/components/EducationTest.js
@@ -0,0 +1,11 @@
+import Adapter from 'enzyme-adapter-react-16';
+import {shallow, configure} from 'enzyme';
+import Education from './components/Education';
+
+configure({adapter: new Adapter()});
+
+// describe e it son funciones de testing, ambas toman un string que habla o escribe lo que vas a testear, ambas aceptan una funcionn anonima como segundo parametro. it() dentro de su parametro de funcion anonima realiza el testeo
+
+describe('Education Component', ()=> {
+    it()
+})
